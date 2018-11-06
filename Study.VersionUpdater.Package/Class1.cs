b3e31969831061cfd26883fbcd8c637fc805c3a1
ebl <contact@ebl-consulting.com>
2018-11-06 20:43:42 +0100

    Add project files.

---
+++ Study.VersionUpdater.Package/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.VersionUpdater.Package
+{
+    public class Class1
+    {
+    }
+}
